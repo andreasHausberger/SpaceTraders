dd392bbf200db1eeb47d52dfa17ecb55a8ab7ddc
Andreas Hausberger <hausbergerandreas1@gmail.com>
2021-03-06 11:02:47 +0100

    ClaimUsername, Custom Button

--- Shared/View/ClaimUsernameView.swift
+++ Shared/View/ClaimUsernameView.swift
@@ -2,15 +2,54 @@
 //  ClaimUsernameView.swift
 //  SpaceTraders
 //
-//  Created by Andreas Hausberger on 05.03.21.
+//  Created by Andreas Hausberger on 06.03.21.
 //
 
 import SwiftUI
 import Combine
 
 struct ClaimUsernameView: View {
+    @State var newUsername = ""
+    @State var subscriptions: Set<AnyCancellable> = []
+    @State var postUsernameResponse: UsernameResponse?
+    @State var isAvailable: Bool = false
+    
+    let api = SpaceTradersAPI.shared
     
     var body: some View {
-        Text("")
+        Form {
+            Section {
+                TextField("Claim a new Username", text: $newUsername)
+                SpacyButton(color: .black) {
+                    api.postUsername(username: newUsername)?
+                        .sink(receiveCompletion: { (completion) in
+                            switch completion {
+                            case .finished: break
+
+                            case .failure(let error):
+                                print("Error \(error)")
+                                self.isAvailable = false
+                            }
+                        }, receiveValue: { (response) in
+                            self.postUsernameResponse = response
+                        })
+                        .store(in: &subscriptions)
+                    
+                }
+                HStack {
+                    Text("Availability: ")
+                
+                }
+                
+            }
+        }
+        .navigationTitle("Claim Username")
+        
+    }
+}
+
+struct ClaimUsernameView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClaimUsernameView()
     }
 }
---
+++ Shared/View/Component/SpacyButton.swift
@@ -0,0 +1,36 @@
+//
+//  SpacyButton.swift
+//  SpaceTraders
+//
+//  Created by Andreas Hausberger on 06.03.21.
+//
+
+import SwiftUI
+
+struct SpacyButton: View {
+    var color: Color
+    var textColor: Color? = .white
+    var image: Image?
+    var text: String?
+    var action: () -> Void
+    var body: some View {
+        
+        Button(action: action, label: {
+            Text("\(text?.uppercased() ?? "BUTTON")")
+                .foregroundColor(textColor)
+                .padding(.horizontal, 32)
+                .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .background(color)
+                .cornerRadius(10)
+                
+        })
+    }
+}
+
+struct SpacyButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacyButton(color: .green) {
+            print("Example")
+        }
+    }
+}
